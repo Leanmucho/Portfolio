c3280b16ac61cc1050d1547216ad8adb2c51bbd5
Leanmucho <acunaleandro3@gmail.com>
2021-11-14 14:45:17 -0300

    Commit para menu hamburguesa(falta terminar) y algunos estilos para el menu de contacto

--- js/app.js
+++ js/app.js
@@ -13,3 +13,23 @@ function handleSubmit(event){
     $buttonMailto.setAttribute('href', `mailto:acunaleandro3@gmail.com?subject=${form.get('name')}${form.get('email')}&body=${form.get('msg')}`)
     $buttonMailto.click()
 }
+
+//boton menu hamburguesa
+
+const iconoMenu = document.querySelector('#icono-menu')
+const menu = document.querySelector('#menu')
+
+iconoMenu.addEventListener("click", (e)=>{
+    //Alternamos los estilos para el menu y el body
+    menu.classList.toggle('active')
+    document.body.classList.toggle('opacity')
+
+    // //alternamos su atributo 'src' para el icono del menu
+    // const rutaActual = e.target.getAttribute('src');
+
+    // if(rutaActual == 'img/open-menu.png'){
+    //     e.target.setAttribute('src', 'img/open-menu2.png')
+    // } else {
+    //     e.target.setAttribute('src','img/open-menu.png')
+    // }
+})
