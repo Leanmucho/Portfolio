37f4309fe923c5ebb01b288291341882d14379f2
Leanmucho <acunaleandro3@gmail.com>
2021-12-07 11:31:42 -0800

    Header arreglado

--- js/app.js
+++ js/app.js
@@ -16,20 +16,14 @@ function handleSubmit(event){
 
 //boton menu hamburguesa
 
-const iconoMenu = document.querySelector('#icono-menu')
-const menu = document.querySelector('#menu')
-
-iconoMenu.addEventListener("click", (e)=>{
-    //Alternamos los estilos para el menu y el body
-    menu.classList.toggle('active')
-    document.body.classList.toggle('opacity')
-
-    // //alternamos su atributo 'src' para el icono del menu
-    // const rutaActual = e.target.getAttribute('src');
-
-    // if(rutaActual == 'img/open-menu.png'){
-    //     e.target.setAttribute('src', 'img/open-menu2.png')
-    // } else {
-    //     e.target.setAttribute('src','img/open-menu.png')
-    // }
-})
+jQuery('document').ready(function($){
+    let menuBtn = $('.menu-icon');
+    let menu = $('.navigation ul');
+    menuBtn.click(function(){
+    if(menu.hasClass('show')){
+        menu.removeClass('show')
+    }else{
+        menu.addClass('show')
+    }
+    })
+    });
