6197d9fe57d14558067b43cad987ca96355b293f
Leanmucho <acunaleandro3@gmail.com>
2021-12-07 15:27:23 -0800

    Test netlify

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 const $form = document.querySelector('#form')
 const $buttonMailto = document.querySelector('#mail')
 
-$form.addEventListener('submit', handleSubmit)
+// $form.addEventListener('submit', handleSubmit)
 
 
 function handleSubmit(event){
     event.preventDefault()
-    const form = new FormData(this)
-    console.log(form.get('name'))
-    console.log(form.get('email'))
-    console.log(form.get('msg'))
-    $buttonMailto.setAttribute('href', `mailto:acunaleandro3@gmail.com?subject=${form.get('name')}${form.get('email')}&body=${form.get('msg')}`)
-    $buttonMailto.click()
+    // const form = new FormData(this)
+    // console.log(form.get('name'))
+    // console.log(form.get('email'))
+    // console.log(form.get('msg'))
+    // $buttonMailto.setAttribute('href', `mailto:acunaleandro3@gmail.com?subject=${form.get('name')}${form.get('email')}&body=${form.get('msg')}`)
+    // $buttonMailto.click()
 }
 
 //boton menu hamburguesa
